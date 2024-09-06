fc2f0261102bb7f20178acb17fc1f14b4a069392
Mikołaj Błaż <mblaz@nvidia.com>
2024-09-06 10:58:16 +0200

    Apply formatting

--- tests/unit_tests/dist_checkpointing/test_fp8.py
+++ tests/unit_tests/dist_checkpointing/test_fp8.py
@@ -23,10 +23,13 @@ from megatron.core.dist_checkpointing.optimizer import (
     get_param_id_to_sharded_param_map,
     optim_state_to_sharding_state,
 )
-from megatron.core.dist_checkpointing.serialization import \
-    get_default_save_sharded_strategy, get_default_load_sharded_strategy
+from megatron.core.dist_checkpointing.serialization import (
+    get_default_load_sharded_strategy,
+    get_default_save_sharded_strategy,
+)
 from megatron.core.dist_checkpointing.strategies.fully_parallel import (
-    FullyParallelSaveStrategyWrapper, FullyParallelLoadStrategyWrapper,
+    FullyParallelLoadStrategyWrapper,
+    FullyParallelSaveStrategyWrapper,
 )
 from megatron.core.dist_checkpointing.utils import extract_sharded_tensors
 from megatron.core.tensor_parallel import model_parallel_cuda_manual_seed
@@ -51,7 +54,9 @@ class TestFP8:
 
         def get_ten(dtype: str = 'fp8'):
             if dtype == 'fp8':
-                return Float8Tensor.to_float8(torch.full((3,), Utils.rank, dtype=torch.bfloat16, device='cuda'))
+                return Float8Tensor.to_float8(
+                    torch.full((3,), Utils.rank, dtype=torch.bfloat16, device='cuda')
+                )
             elif dtype == 'bf16':
                 return torch.full((3,), Utils.rank, dtype=torch.bfloat16, device='cuda')
             elif dtype == 'fp16':
@@ -68,41 +73,42 @@ class TestFP8:
         torch.distributed.broadcast(ten, src=src_rank)
         assert torch.all(ten == src_rank)
 
-
     @pytest.mark.parametrize(
-        ('use_fpsl', 'src_tp_pp', 'dest_tp_pp', 'load_exchange_algo',),
+        ('use_fpsl', 'src_tp_pp', 'dest_tp_pp', 'load_exchange_algo'),
         [
             (True, (2, 4), (2, 4), 'broadcast'),
             (True, (2, 4), (2, 4), 'gather_rounds'),
             (False, (2, 4), (2, 4), None),
-        ]
+        ],
     )
-    def test_fp8_save_load(self, tmp_path_dist_ckpt, use_fpsl, src_tp_pp, dest_tp_pp, load_exchange_algo):
+    def test_fp8_save_load(
+        self, tmp_path_dist_ckpt, use_fpsl, src_tp_pp, dest_tp_pp, load_exchange_algo
+    ):
         Utils.initialize_model_parallel(*src_tp_pp)
 
         def get_fp8_tensor(fill_val=1):
-            return Float8Tensor.to_float8(torch.full((3,), fill_val, dtype=torch.bfloat16, device='cuda'))
+            return Float8Tensor.to_float8(
+                torch.full((3,), fill_val, dtype=torch.bfloat16, device='cuda')
+            )
 
         def get_state_dict(fill_val=1):
             return {
-                'a': ShardedTensor.from_rank_offsets('a', get_fp8_tensor(fill_val), (0, Utils.rank, Utils.world_size), replica_id=0),
-                'b': ShardedTensor.from_rank_offsets('b', get_fp8_tensor(fill_val), replica_id=Utils.rank),
-                'c': ShardedTensor.from_rank_offsets('c', get_fp8_tensor(fill_val), replica_id=Utils.rank),
+                'a': ShardedTensor.from_rank_offsets(
+                    'a', get_fp8_tensor(fill_val), (0, Utils.rank, Utils.world_size), replica_id=0
+                ),
+                'b': ShardedTensor.from_rank_offsets(
+                    'b', get_fp8_tensor(fill_val), replica_id=Utils.rank
+                ),
+                'c': ShardedTensor.from_rank_offsets(
+                    'c', get_fp8_tensor(fill_val), replica_id=Utils.rank
+                ),
             }
 
         with TempNamedDir(tmp_path_dist_ckpt / 'test_fp8_save_load', sync=True) as ckpt_dir:
             save_strategy = get_default_save_sharded_strategy()
             if use_fpsl:
-                save_strategy = FullyParallelSaveStrategyWrapper(
-                    save_strategy,
-                    None,
-                    True,
-                )
-            save(
-                get_state_dict(4),
-                ckpt_dir,
-                save_strategy,
-            )
+                save_strategy = FullyParallelSaveStrategyWrapper(save_strategy, None, True)
+            save(get_state_dict(4), ckpt_dir, save_strategy)
 
             Utils.destroy_model_parallel()
             Utils.initialize_model_parallel(*dest_tp_pp)
@@ -110,10 +116,7 @@ class TestFP8:
             if use_fpsl:
                 load_strategy = get_default_load_sharded_strategy(ckpt_dir)
                 load_strategy = FullyParallelLoadStrategyWrapper(
-                    load_strategy,
-                    None,
-                    False,
-                    load_exchange_algo
+                    load_strategy, None, False, load_exchange_algo
                 )
             else:
                 load_strategy = None
